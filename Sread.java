3828fcef20bafc757d41ff8b5f3617f7b59ffb14
Tri-Tech <craig.coleman@ksd.org>
2016-09-30 07:44:52 -0700

    Update and rename Bread.java to Sread.java

--- Sread.java
+++ Sread.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-class Bread {
+class Sread {
 
 	public static void main(String[] args) {
 		//variables (cwc)
